30fdafeef2704302b806cae36ed1d3ede540ee86
Sam Siles <sam_siles@yahoo.com>
2021-04-30 15:32:51 -0400

    finished homework 7

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -4,17 +4,25 @@ function mayuscula(nombre) {
   //La función recibe un nombre y debe devolver el mismo que recibe pero con su primer letra en mayúscula
   //ej: Recibe "mario" ----> Devuelve "Mario"
   //Tu código:
+  //let div= nombre.split('');
+  //may=div[0].toUppercase();
+  //div.unshift(may);
+  //res=div.join('');
+  //return res;
+  return nombre.charAt(0).toUpperCase() + nombre.slice(1);
 }
 
 function invocarCallback(cb) {
   // Invoca al callback `cb`
   //Tu código:
+  return cb();
 }
 
 function operacionMatematica(n1, n2, cb) {
   //Vamos a recibir una función que realiza una operación matemática como callback junto con dos números.
   //Devolver el callback pasándole como argumentos los números recibidos.
   //Tu código:
+  return cb(n1, n2);
 }
 
 function sumarArray(numeros, cb) {
@@ -22,12 +30,20 @@ function sumarArray(numeros, cb) {
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
   //Tu código:
+  res=numeros.reduce(function(acumulador, elemento){
+    return acumulador + elemento;
+  });
+  cb(res);
+  //return cb(res);
 }
 
 function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
   //Tu código:
+  array.forEach(function (elemento, indice){
+    cb(elemento);
+  });
 }
 
 function map(array, cb) {
@@ -35,12 +51,24 @@ function map(array, cb) {
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   //Tu código:
+  nuevo=[];
+  array.forEach(function (elemento, indice){
+    nuevo.push(cb(elemento));
+  });
+  return nuevo;
 }
 
 function filter(array) {
   //Filtrar todos los elementos del array que comiencen con la letra "a".
   //Devolver un nuevo array con los elementos que cumplen la condición
   //Tu código:
+  nuevo=[];
+  array.forEach(function (elemento, indice){
+    if(array[indice].charAt(0)=== "a"){
+      nuevo.push(elemento);
+    }
+  });
+  return nuevo;
 }
 
 // No modificar nada debajo de esta línea
